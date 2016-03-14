5c13477fc49f9de7c5cd47315a634a4c62304db2
YellowFish <theo.costantini@gmail.com>
2016-03-14 15:18:41 +0100

    Change index.html to index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
-<html ng-app>
+<html>
 	<head>
 		<meta charset="utf-8">
 
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		<script src="assets/js/jquery.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/js/angular.min.js"></script>
+		<script src="assets/js/jquery.history.js"></script>
+		<script src="assets/js/app.js"></script>
 	</head>
 	<body>
+		<header>
+
+		</header>
+		<div id="content">
 		
+		</div>
+		<footer>
+			
+		</footer>
 	</body>
 </html>
