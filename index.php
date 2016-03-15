d6bce32b0b601981a4f2d1ddb1c42b1f4d1e63ab
YellowFish <theo.costantini@gmail.com>
2016-03-15 12:19:54 +0100

    Fix bug with asset path caused by url rewriting

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
+<?php
+	include('config.php');
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/js/jquery.history.js"></script>
-		<script src="assets/js/app.js"></script>
+		<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+		<script src="/assets/js/jquery.min.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
+		<script src="/assets/js/jquery.history.js"></script>
+		<script src="/assets/js/app.js"></script>
 	</head>
 	<body>
 		<header>
-
+			<a href="/articles" title="Articles">Articles</a>
 		</header>
 		<div id="content">
 		
