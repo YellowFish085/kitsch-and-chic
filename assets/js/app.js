41f634f358386e13a40f2aa7eec3932a42299014
YellowFish <theo.costantini@gmail.com>
2016-03-15 12:21:12 +0100

    Evil commit - Fix bug with asset path caused by url rewrite - Add display for articles and article

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ var Article = (function() {
 			// Public
 			loadArticles: function() {
 				return	$.ajax({
-									url: 'assets/json/articles.json',
+									url: '/assets/json/articles.json',
 									cache: false,
 									contentType: "application/json; charset=utf-8",
 									dataType: "json"
@@ -30,7 +30,13 @@ var Article = (function() {
 			},
 
 			find: function (id) {
-				return id;
+				var r = null;
+				$.each(articles.articles, function(i, v) {
+			    if (v.id == id) {
+			        r = v;
+			    }
+				});
+				return r;
 			}
 		};
 	};
@@ -53,14 +59,75 @@ $(document).ready( function() {
 	articles = Article.getInstance();
 	articles.loadArticles()
 		.done( function(r) {
-			console.log(articles.all());
+			processDisplay(articles);
 		})
 		.fail( function(r) {
 			alert('Could not load the json file.');
 		});
 
-	$('a').on('click', function(e) {
+	$('body').on('click', 'a', function(e) {
 		e.preventDefault();
 		History.pushState(null, e.target.title, e.target.href);
 	});
-});			
+});
+
+function processDisplay(articles) {
+	var params = getUrlParameters();
+
+	if (params[0] == 'articles' || params.length == 0) {
+		console.log('// show articles');
+		displayAllArticles(articles);
+	}
+	else if (params[0] == 'article' && params.length == 2) {
+		console.log('// show article');
+		displayArticle(articles, params[1]);
+	}
+}
+
+function displayAllArticles(articles) {
+	$.ajax({
+		url: '/ajax/article.php',
+		type: 'POST',
+		data: {action: 'list', data: articles.all()}
+	})
+		.done( function(data) {
+	    $("#content").append(data);
+		});
+}
+
+function displayArticle(articles, id) {
+	var article = articles.find(id);
+
+	$.ajax({
+		url: '/ajax/article.php',
+		type: 'POST',
+		data: {action: 'show', data: article}
+	})
+		.done( function(data) {
+	    $("#content").append(data);
+		});
+}
+
+function getUrlParameters() {
+	var params = window.location.pathname.split("/");
+
+	while (params[0] == "") {
+		params.shift();
+	}
+	while (params[params.length-1] == "") {
+		params.pop();
+	}
+
+	return params;
+}
+
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	url = url.toLowerCase(); // This is just to avoid case sensitiveness  
+	name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	    results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
