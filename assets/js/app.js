883dd9387533595e0109946231f990abf0952d38
YellowFish <theo.costantini@gmail.com>
2016-03-20 16:31:15 +0100

    Big changes on app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,102 +1,93 @@
-var Article = (function() {
-	var instance;
-
-	function init() {
-		// Singleton
-		// Private
-		var articles;
-
-		return {
-			// Public
-			loadArticles: function() {
-				return	$.ajax({
-									url: '/assets/json/articles.json',
-									cache: false,
-									contentType: "application/json; charset=utf-8",
-									dataType: "json"
-								})
-									.then( function(data) {
-								    articles = data
-								    return true;
-									},
-									function(data) {
-										console.log(data.responseText);
-										return false;
-									});
-			},
-
-			all: function () {
-				return articles;
-			},
-
-			find: function (id) {
-				var r = null;
-				$.each(articles.articles, function(i, v) {
-			    if (v.id == id) {
-			        r = v;
-			    }
-				});
-				return r;
-			}
-		};
-	};
-
-	return {
-		// Get the singleton instance
-		getInstance: function () {
-			if (!instance) {
-				instance = init();
-				return instance;
-			}
-			else {
-				return instance;
-			}
-		}
-	};
-})();
-
-$(document).ready( function() {
-	articles = Article.getInstance();
-	articles.loadArticles()
-		.done( function(r) {
-			processDisplay(articles);
+$(document).ready(function() {
+	$.when(
+		$.getScript('/assets/js/components/Article.js'),
+		$.getScript('/assets/js/components/TemplateEngine.js'),
+		$.Deferred(function(deferred){
+			$(deferred.resolve)
 		})
-		.fail( function(r) {
-			alert('Could not load the json file.');
-		});
+	).done(function(){
+		initApp()
+	})
+	.fail(function(e) {
+		console.error(e.responseText)
+	})
+})
 
+// Init app
+function initApp() {
+	TemplateEngine.getInstance('content')
+
+	Article.getInstance().loadArticles()
+		.done( function() {
+			processView()
+		})
+		.fail( function() {
+			alert('ERROR: Could not load the json file.')
+		})
+
+	setEvents()
+}
+
+// Events
+function setEvents() {
 	$('body').on('click', 'a', function(e) {
-		e.preventDefault();
-		History.pushState(null, e.target.title, e.target.href);
-	});
-});
+		e.preventDefault()
+		History.pushState(null, e.target.title, e.target.href)
+		processView()
+	})
+}
 
-function processDisplay(articles) {
-	var params = getUrlParameters();
+// Check the url params and call the template method
+function processView() {
+	TemplateEngine.getInstance().displayPreloader()
 
-	if (params[0] == 'articles' || params.length == 0) {
-		console.log('// show articles');
-		displayAllArticles(articles);
+	var params = getUrlParameters()
+
+	if (params.length == 1) {
+		if (params[0] == 'about') {
+			console.Log('display about')
+		}
+		else {
+			console.log('display articles from ' + params[0] + ' category')
+			displayArticles(params[0])
+		}
 	}
-	else if (params[0] == 'article' && params.length == 2) {
-		console.log('// show article');
-		displayArticle(articles, params[1]);
+	else if (params.length == 2) {
+		if (params[0] == 'article') {
+			console.log('display article ' + params[1])
+			displayArticle(params[1])
+		}
+		else {
+			console.log('display articles from ' + params[0] + '/' + params[1] + ' subcategory')
+			displayArticles(params[1])
+		}
+	}
+	else {
+		console.log('display all articles')
+		displayAllArticles()
 	}
 }
 
-function displayAllArticles(articles) {
+function displayAllArticles() {
+	/*
 	$.ajax({
 		url: '/ajax/article.php',
 		type: 'POST',
-		data: {action: 'list', data: articles.all()}
+		data: {action: 'list', data: Article.getInstance().all()}
 	})
 		.done( function(data) {
-	    $("#content").append(data);
-		});
+	    $("#content").html(data)
+		})
+	*/
 }
 
-function displayArticle(articles, id) {
-	var article = articles.find(id);
+function displayArticles(category) {
+	TemplateEngine.getInstance().getTemplate('articles', Article.getInstance().findByCategory(category))
+}
+
+function displayArticle( id) {
+	/*
+	var article = Article.getInstance().find(id)
 
 	$.ajax({
 		url: '/ajax/article.php',
@@ -104,10 +95,12 @@ function displayArticle(articles, id) {
 		data: {action: 'show', data: article}
 	})
 		.done( function(data) {
-	    $("#content").append(data);
-		});
+	    $("#content").html(data)
+		})
+	*/
 }
 
+// Helper methods
 function getUrlParameters() {
 	var params = window.location.pathname.split("/");
 
@@ -119,15 +112,4 @@ function getUrlParameters() {
 	}
 
 	return params;
-}
-
-function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	url = url.toLowerCase(); // This is just to avoid case sensitiveness  
-	name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-	    results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
